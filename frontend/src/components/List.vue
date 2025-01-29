eb7e8a49fa7d33fdb1f31faec233e418a7c9dbb4
teukuriansyah <teukuriansyahkarim@gmail.com>
2025-01-29 23:26:56 +0700

    ecommerce vue first step

---
+++ frontend/src/components/List.vue
@@ -0,0 +1,12 @@
+<template>
+  <li class="flex flex-col border-2 border-black rounded p-2 gap-2 list-none">
+    <img :src="img" :alt="title" class="aspect-square object-contain"/>
+    <h2 class="truncate text-lg">{{ title }}</h2>
+    <h1 class="font-bold text-xl">Rp {{ numberWithCommas(price*16000) }}</h1>
+  </li>
+</template>
+
+<script setup lang="ts">
+  defineProps(["title","img","price"]);
+  const numberWithCommas = (x) => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+</script>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,5 @@
+<template>
+  <nav class="bg-black p-2">
+    <a href="/" class="text-2xl text-white font-bold">Ecommerce</a>
+  </nav>
+</template>
